f4136025b8e0efffc0a45b41ffd8bae493aea708
Félix MARQUET <72651575+BreizhHardware@users.noreply.github.com>
2022-07-28 21:42:16 +0200

    Update zsh_custom.sh

--- zsh_custom.sh
+++ zsh_custom.sh
@@ -4,3 +4,4 @@ wget https://raw.githubusercontent.com/BreizhHardware/post_install/main/.zshrc
 cp .zshrc ~/.zshrc
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
+rm -rf .zshrc
