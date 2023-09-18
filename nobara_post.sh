0ca77732b1ad263d1b3dd7ca3a765e16917c571c
Félix MARQUET <72651575+BreizhHardware@users.noreply.github.com>
2023-09-18 17:13:58 +0200

    Update nobara_post.sh

--- nobara_post.sh
+++ nobara_post.sh
@@ -12,7 +12,7 @@ then
 fi 
 
 dnf -y --nogpgcheck --refresh upgrade
-dnf install -y gnome-shell-extension-dash-to-dock htop nmon inxi figlet btop nvtop google-noto-fonts
+dnf install -y gnome-shell-extension-dash-to-dock htop nmon inxi figlet btop nvtop google-noto-fonts-common
 dnf install -y gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel
 dnf install -y lame\* --exclude=lame-devel
 dnf group upgrade -y --with-optional Multimedia
